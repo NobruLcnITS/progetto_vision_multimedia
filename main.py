1a8a2fce5658c03effd8945cb21f34c41a0cc38e
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-09-18 15:07:15 +0200

    agg

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
 import os
 import cv2 as cv
 import numpy as np
-from keras.models import load_model
 import matplotlib.pyplot as plt
 import pandas as pd
+import gradio as gr
 
 c_fucsia = (255, 0, 255)
 c_green = (0, 255, 0)
 model_path = r'..\model\lego_1_04.keras'
 predict_path = 'Data\\predict'
 
-def plot_images(images, predicted_labels, num_images, class_names):
-    plt.figure(figsize=(24, 24))
-    for i in range(num_images):
-        plt.subplot(4, 5, i + 1)
-        plt.imshow(images[i])
-        predicted_label = class_names[predicted_labels[i]]
-        plt.title(f'Pred: {predicted_label}', fontsize=20)
-        plt.axis('off')
-    plt.tight_layout()
-    plt.show()
 
-
-if __name__ == '__main__':
-    filename = r'Data\img\Lego_2.JPG'
-    if not os.path.exists(filename):
-        print('Lego messi nella cartella sbagliata')
-        exit(1)
+def process(img: np.ndarray) -> list:
+    
+    if img.shape > (400, 800, 3):
+        img_resize = cv.resize(img , (850,850))
+    else :
+        img_resize = img.copy()
         
-    print('Carico Lego')
-    
-    img = cv.imread(filename)
-    img_resize = cv.resize(img , (850,850))
-    
-    gray = cv.cvtColor(img_resize, cv.COLOR_BGR2GRAY)
-    
-    cv.imshow('Originale', img_resize)
-    cv.imshow('Scala di grigi', gray)
+    img_resize_RGB = cv.cvtColor(img_resize, cv.COLOR_BGR2RGB)
+    gray = cv.cvtColor(img_resize_RGB, cv.COLOR_BGR2GRAY)
     
     print(f'Media: {np.mean(gray)}')
     avg = np.mean(gray)
     
     if avg > 125:
         flag = cv.THRESH_BINARY_INV
+        
     else:
         flag = cv.THRESH_BINARY
         
@@ -53,13 +37,9 @@ if __name__ == '__main__':
                                     blockSize=11,
                                     C=6)
     
-    final = cv.medianBlur(adaptive, 7)
-    cv.imshow('Adaptive Threshold', adaptive)
-    cv.imshow('Adaptive Threshold Blurred', final)
-    
     blur = cv.GaussianBlur(gray, (7, 7), 0)
     _, otsu = cv.threshold(blur, 0, 255, flag + cv.THRESH_OTSU)
-    cv.imshow('Otsu\'s Threshold Blurred', otsu)
+   
 
     contours, hierarchy = cv.findContours(otsu,
                                           mode=cv.RETR_EXTERNAL,
@@ -67,9 +47,11 @@ if __name__ == '__main__':
                                           )
     
     
-    check = img_resize.copy()
+    check = img_resize_RGB.copy()
     c_black = (0, 0, 0)
-    check[::] = c_black
+    check[::] = c_black 
+    
+    targets = []
     valid_contours = []
     
     for i, cnt in enumerate(contours):
@@ -83,45 +65,36 @@ if __name__ == '__main__':
                    org=(text_x, text_y),
                    fontFace=cv.FONT_HERSHEY_SIMPLEX,
                    fontScale=1,
-                   color=c_fucsia,
-                   thickness=3)
-        
-        target = img_resize[y-20:y+h+20,x-20:x+w+20]
-        cv.drawContours(check , [cnt], -1, (0, 0, 255), 3)
-        
-        cv.imwrite(f"{predict_path}\\target_{i}.png", target)
-        cv.imshow(f'target: {i}', target)
-        
-        cv.drawContours(check, [cnt], 0, c_green, 3)
+                   color=c_fucsia,  
+                   thickness=3) 
+        x1 = max(0, x - 20)
+        y1 = max(0, y - 20)
+        x2 = min(img_resize_RGB.shape[1], x + w + 20)
+        y2 = min(img_resize_RGB.shape[0], y + h + 20)
+        target = img_resize_RGB[y1: y2, x1:x2]
+        cv.drawContours(check , [cnt], -1, (0, 255, 0), 3)
         valid_contours.append(cnt)
         
         
-    cv.imshow('Check', check)
-    
-    img3 = cv.drawContours(img_resize, valid_contours, -1, c_green, 3)
-    cv.imshow('Contorni', img3)
+        #cv.imwrite(f"{predict_path}\\target_{i}.png", target)
 
-    print(f'Lego trovati: {len(valid_contours)}')
-
-    
-    df = pd.read_csv(r'./validation.csv', header=0)
-    df_selected = df.groupby('Id').first()
-
-    labels = df_selected['Description'].values
-    print(labels)
-
-    model = load_model(model_path)
-    
-    img_path = []
-    for img in os.listdir(predict_path):
-        img_path = os.path.join(img, predict_path)
-    
-    y_hat_pre = model.predict(img_path)
-
-    predicted_labels = y_hat_pre.argmax(axis=1)
+        target_RGB= cv.cvtColor(target, cv.COLOR_BGR2RGB)
         
-    num_images = min(20, len(img_path))
-    plot_images(img_path, predicted_labels, num_images, labels)
+        targets.append(target_RGB)
     
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    test = np.median(targets[1], axis=-1)
+        
+    contorni = cv.drawContours(img_resize_RGB, valid_contours, -1, c_green, 3)
+    contorni = cv.cvtColor(contorni, cv.COLOR_BGR2RGB)    
+     
+    return [img_resize,gray,adaptive,otsu,check,contorni]+targets, test
+
+
+if __name__ == '__main__':
+    demo = gr.Interface(
+    fn=process,
+    inputs=['image'],
+    outputs=[gr.Gallery(),'text'],
+    )
+        
+    demo.launch(share=True)
